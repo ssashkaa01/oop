5c903ad5a9ffc60f60e02575df92613d0e200f51
STORYM <ssashkaa01@gmail.com>
2019-07-19 23:00:08 +0300

    add hw

--- 19.07.2019/19.07.2019/Source.cpp
+++ 19.07.2019/19.07.2019/Source.cpp
@@ -22,8 +22,20 @@ public:
 		return tmp;
 	}
 
-	T get() {
-		return *ptr;
+	T* get() {
+		return ptr;
+	}
+
+	void reset(my_unique_ptr <T>& other) {
+		
+		if(ptr != nullptr) delete ptr;
+
+		if (other.ptr != nullptr) {
+			ptr = other.release();
+		}
+		else {
+			ptr = 0;
+		}
 	}
 
 	T operator * () const
@@ -44,7 +56,7 @@ public:
 		else return false;
 	}
 
-	T &operator=(unique_ptr <T>&&other) {
+	T &operator=(my_unique_ptr <T>&&other) {
 		if (other.ptr != nullptr) {
 
 			if (this->ptr != nullptr) {
@@ -56,52 +68,102 @@ public:
 		}
 		return *this;
 	}
-	
-	my_unique_ptr<T> *operator->() {
-		return *ptr;
+
+	T* operator->() {
+		return ptr;
 	}
 };
 
+template <class T>
+class my_shared_ptr
+{
+private:
+	T *ptr;
+
+public:
+	my_shared_ptr(T *ptr) : ptr(ptr) {
+
+	}
+	my_shared_ptr() {
+
+	}
+	~my_shared_ptr() {
+		delete ptr;
+	}
+
+	T* release() {
+		T *tmp = ptr;
+		ptr = nullptr;
+		return tmp;
+	}
+
+	T* get() {
+		return ptr;
+	}
+
+	void reset(my_shared_ptr <T>& other) {
+
+		if (ptr != nullptr) delete ptr;
+
+		if (other.ptr != nullptr) {
+			ptr = other.release();
+		}
+		else {
+			ptr = 0;
+		}
+	}
+
+	T operator * () const
+	{
+		return *ptr;
+	}
+
+	void swap(my_shared_ptr& other) {
+		T *tmp = ptr;
+		ptr = other.get();
+		other.ptr = tmp;
+	}
+
+	explicit operator bool() const {
+		if (ptr != nullptr) {
+			return true;
+		}
+		else return false;
+	}
+
+	my_shared_ptr<T>& operator= (const my_shared_ptr<T> &other)
+	{
+		this->ptr = other.ptr;
+	
+		return *this;
+	}
+
+	T* operator->() {
+		return ptr;
+	}
+};
 
 int main() {
 
-	/*
-	Створіть свої власні класи розумних вказівників.
-	Один з класів повинен повторювати функціональність стандартного класу unique_ptr, а інший - shared_ptr.
-	Напишіть код по тестуванню ваших класів.
-
-	Обов'язково потрібно реалізувати наступний функціонал:
-
-	Конструктори (параметризовані, по замовчуванню)
-	Деструктор
-	Оператори розіменовування (* ->)
-	Перевантаження оператора приведення типа до bool (повертає true, ящко об'єкт володіє адресою)
-	Перевантаження оператора присвоєння (=)
-
-	Та методи (для unique_ptr та shared_ptr їх перелік дещо відрізняється), ось деякі з них:
-	Get - повертає адресу якою володіє об'єкт
-	Release - також повертає адресу, знімаючи володіння нею з себе
-	Reset - приймає нову адресу та перевстановлює володіння на неї, параметр методу має значення по замовчуванню, яке встановлює нову адресу в 0
-	Swap - приймає такий ж об'єкт і замінює адреси на які вони вказують
-	*/
-
-	my_unique_ptr<int> ukaz(new int(4));
-	my_unique_ptr<int> ukaz2(new int(5));
+	/*my_unique_ptr<int> ukaz(new int(5));
+	my_unique_ptr<int> ukaz2(new int(6));*/
 
 	//ukaz2.swap(ukaz);
-	//(&ukaz)->get();
-	ukaz->get();
-	//cout << *(ukaz.get()) << endl;
-	//cout << *(ukaz.get()) << endl;
+	//cout << *(ukaz.get());
+	//cout << ukaz.get();
+	
+	//my_unique_ptr<int> ukaz3 = ukaz.release();
 
-	my_unique_ptr<int> ukaz3 = ukaz.release();
+	/*ukaz.reset(ukaz2);
 
 	if (ukaz) {
 		cout << "isset" << endl;
 	}
 	else {
 		cout << "not isset" << endl;
-	}
+	}*/
+	//my_shared_ptr<int> ukaz(new int(5));
+
 
 	system("pause");
 	return 0;
